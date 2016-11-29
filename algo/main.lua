8d1440503551bbc00aba04c9404375a79f990d86
Michael Vetter <jubalh@openmailbox.org>
2016-11-29 10:45:45 +0100

    algo: create obstacles and a goal

--- algo/main.lua
+++ algo/main.lua
@@ -1,8 +1,27 @@
 square_size = 40
 grid_size = 10
-player = {x,y}
+player = {x=nil,y=nil}
+goal = {x=nil,y=nil}
 maze = {}
 
+function setObjectPosition(obj, x, y, sign)
+	if obj.x ~= nil then
+		maze[obj.x][obj.y] = ' '
+	end
+	obj.x = x
+	obj.y = y
+	maze[x][y] = sign
+end
+
+function setPlayerPosition(x, y)
+	setObjectPosition(player, x, y, 'p')
+end
+
+function setGoalPosition(x, y)
+	setObjectPosition(goal, x, y, 'g')
+end
+
+--[[
 function setPlayerPosition(x, y)
 	if player.x ~= nil then
 		maze[player.x][player.y] = ' '
@@ -10,6 +29,16 @@ function setPlayerPosition(x, y)
 	player.x = x
 	player.y = y
 	maze[x][y] = 'p'
+end
+--]]
+
+function create_obstacles()
+	for n=4,6 do
+		maze[3][n] = 'w'
+	end
+	for n=4,6 do
+		maze[n][6] = 'w'
+	end
 end
 
 function love.load()
@@ -31,6 +60,9 @@ function love.load()
 	-- set player position
 	setPlayerPosition(2, 2)
 	love.graphics.setBackgroundColor(40,128,33)
+	-- make some obstacles
+	create_obstacles()
+	setGoalPosition(9, 7)
 end
 
 function love.draw()
@@ -47,6 +79,10 @@ function love.draw()
 				love.graphics.setColor(255,0,0)
 				love.graphics.rectangle('fill', x, y, square_size, square_size)
 				love.graphics.setColor(255,255,255)
+			elseif maze[i][j] == 'g' then
+				love.graphics.setColor(255,233,0)
+				love.graphics.rectangle('fill', x, y, square_size, square_size)
+				love.graphics.setColor(255,255,255)
 			end
 		y = y + 50
 		end
@@ -56,8 +92,21 @@ end
 
 function love.keypressed(key)
 	if key == 'escape' then
-		love.event.push('quit')
-	elseif key == 's' then
-		setPlayerPosition(player.x, player.y+1)
+	elseif key == 's' or key == 'down' then
+		if maze[player.x][player.y+1] == ' ' then
+			setPlayerPosition(player.x, player.y+1)
+		end
+	elseif key == 'w' or key == 'up' then
+		if maze[player.x][player.y-1] == ' ' then
+			setPlayerPosition(player.x, player.y-1)
+		end
+	elseif key == 'd' or key == 'right' then
+		if maze[player.x+1][player.y] == ' ' then
+			setPlayerPosition(player.x+1, player.y)
+		end
+	elseif key == 'a' or key == 'left' then
+		if maze[player.x-1][player.y] == ' ' then
+			setPlayerPosition(player.x-1, player.y)
+		end
 	end
 end
